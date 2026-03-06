7f9b2bff0d9abefe62488ca0fac2bbbc0782f11b
sage <jjhcat0402@gmail.com>
2026-03-06 21:18:06 +0900

    Create sample.c

---
+++ aslr/sample.c
@@ -0,0 +1,18 @@
+// filename: sample.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void vuln() {
+    char buf[24];
+    puts("input:");
+    gets(buf);
+}
+
+int main() {
+    setregid(getegid(), getegid());
+    setvbuf(stdout, NULL, _IONBF, 0);
+    vuln();
+    puts("done");
+    return 0;
+}
